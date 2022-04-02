51c2dc29deaa12e6ae556701761695b9f99652c0
judegiordano <judegiordano@gmail.com>
2022-04-02 01:06:08 -0400

    Shortcut Token

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -4,9 +4,7 @@ import { UnauthorizedError, ForbiddenError, JwtPayload } from "../types";
 import { jwt, config } from "../services";
 
 export async function onRequest(req: FastifyRequest) {
-	const auth = req.headers.authorization;
-	if (!auth) throw new UnauthorizedError();
-	const token = auth.split(" ")[1];
+	const token = req.headers.authorization?.split(" ")[1];
 	if (!token) throw new UnauthorizedError();
 	const payload = jwt.verify<JwtPayload>(token);
 	if (!payload.is_valid || payload.stage !== config.STAGE) throw new ForbiddenError();
